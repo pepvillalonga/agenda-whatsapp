c36973c245429525cbaff2dacba3a14c83d4a58d
Pep <pepvillalonga312345@gmail.com>
2025-12-16 05:13:44 +0100

    .

--- main.py
+++ main.py
@@ -81,5 +81,5 @@ async def handle_message(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
